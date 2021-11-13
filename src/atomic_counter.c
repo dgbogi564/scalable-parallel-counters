fce34c3432166cf93f68793262dd26285b118ea0
dgbogi564 <82741370+dgbogi564@users.noreply.github.com>
2021-11-13 18:51:40 -0500

    partial implementation of naive counter

--- src/atomic_counter.c
+++ src/atomic_counter.c
@@ -1,11 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <pthread.h>
 #include <sys/time.h>
+#include <string.h>
+#include <math.h>
 
 #define COUNTER_VALUE (1UL << 24)
 
-int main(int argc, char** argv) {
+int counter;
 
-	return 0;
+void count() {
+    while(counter < COUNTER_VALUE) {
+        counter++;
+    }
 }
+
+int main(int argc, char** argv) {
+    /* check argument */
+    if(argc < 2) {
+        printf("no argument supplied\n");
+        exit(0);
+    }
+    errno = 0;
+    long int num_threads = strtol(argv[1], NULL, 10);
+    if(num_threads == 0) {
+        printf("%s is not a positive integer\n", strerror(errno));
+    }
+
+    /* create thread array */
+    pthread_t *th_id = malloc(sizeof(pthread_t)*num_threads);
+
+    /* start timer */
+    clock_t start, end;
+    start = clock();
+
+    /* create threads and run counting function */
+    for(int x = 0; x < num_threads; x++) {
+        pthread_create(&th_id[x], NULL, (void *(*)(void *)) count, NULL);
+    }
+    for(int x = 0; x < num_threads; x++) {
+        pthread_join(th_id[x], NULL);
+    }
+
+    /* end timer */
+    end = clock();
+
+    /* get time rounded to 3 decimal places*/
+    double time_ms =  round((end - start)/CLOCKS_PER_SEC)/1000;
+
+    /* print result */
+    printf("Counter finish in ms %f", time_ms);
+    printf("The value of counter should be %d", COUNTER_VALUE);
+    printf("The value of counter is %d", counter);
+
+    /* free */
+    free(th_id);
+
+    /* end program */
+    return 0;
+}
+
